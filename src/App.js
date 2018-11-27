35270a73580cf6706f948566ead4d103fb0fd0a0
Trent Stephens <trentstephens.advertising@gmail.com>
2018-11-26 21:13:31 -0500

    11/26/18 1st update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_RgtK5gJ0kmgt4gwIX1jmtKV_PA-jbrQ",
+    authDomain: "react-chat-rooms.firebaseapp.com",
+    databaseURL: "https://react-chat-rooms.firebaseio.com",
+    projectId: "react-chat-rooms",
+    storageBucket: "react-chat-rooms.appspot.com",
+    messagingSenderId: "195796598816"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+this.state = {
+    rooms: []
+  };
+
+this.roomsRef = this.props.firebase.database().ref('rooms');
+
+class RoomList extends Component {
+  render() {
+    return (
+      <div className="RoomList">
+      This is a test
+      </div>
+    );
+  }
+}
+
+
+
+
+export default RoomList;
